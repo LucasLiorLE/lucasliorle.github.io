5f14b7cd32c07e9f47c3f4a24db704e6ee47f4cb
Lucas Li <LucasLiorLEYT@gmail.com>
2025-09-12 06:48:12 +0000

    I added mobile support but im too lazy to do control + i so lets pray it works

--- static/js/script.js
+++ static/js/script.js
@@ -5,6 +5,14 @@ if (burger) {
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
         burger.classList.toggle('toggle');
+        
+        nav.querySelectorAll('li').forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index * 0.1 + 0.3}s`;
+            }
+        });
     });
 }
 
